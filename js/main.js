5cc09a1d2658d0f5a9112d5ca3564446fac8cd6c
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-02-08 18:53:34 +0100

    Array non popolato

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@ new Vue({
   el: '#root',
   data:{
   disk: [],
+  genreList : [],
+  genreSelect : 'All'
 },
-  methods: {
-
-  },
   mounted(){
     const self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(response => {
       console.log(response)
-      self.disk = response.data.response
+      self.disk = response.data.response;
     })
+  },
+  methods: {
+  filterGenre : function(){
+    const self = this;
+    const optionGenre = [];
+    this.disk.forEach(item => {
+      this.optionGenre.push(item.genre)
+    });
+    this.genreList =  optionGenre.filter((item,index)=>{
+      return optionGenre.indexOf(item) === item
+    })
+
   }
+
+  },
 })
 Vue.config.devtools = true
