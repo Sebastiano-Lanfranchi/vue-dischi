bba92147eb7620a2629a2c6966e85d100e2f6775
Sebastiano-Lanfranchi <sebastiano.lanfranchi@gmail.com>
2021-02-08 21:30:40 +0100

    array genere non si popola

--- js/main.js
+++ js/main.js
@@ -1,31 +1,30 @@
-new Vue({
-  el: '#root',
-  data:{
-  diskList: [],
-  genreList : [],
-
-},
-  mounted(){
-    const self = this;
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-    .then(response => {
-      console.log(response)
-      self.diskList = response.data.response;
-    })
-  },
-  methods: {
-  filterGenre : function(){
-    const self = this;
-    let optionGenre = [];
-    this.diskList.forEach(item => {
-      this.optionGenre.push(item.genre)
-    });
-    this.genreList =  optionGenre.filter((item,index)=>{
-      return optionGenre.indexOf(item) === item
-    })
-
-  }
+  new Vue({
+    el: '#root',
+    data:{
+    diskList: [],
+    genreList : [],
+    genreSelect: 'ALL',
 
   },
-})
-Vue.config.devtools = true
+    mounted(){
+      const self = this;
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(response => {
+        this.diskList = response.data.response;
+      });
+    },
+    methods: {
+    filterGenre : function(){
+      const self = this;
+      let optionGenre = [];
+      this.diskList.forEach(item => {
+        optionGenre.push(item.genre)
+      });
+      this.genreList =  optionGenre.filter((item, index)=>{
+        return optionGenre.indexOf(item) === item
+      })
+
+    },
+    },
+  })
+  Vue.config.devtools = true
